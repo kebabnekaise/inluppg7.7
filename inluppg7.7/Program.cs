4cc6d0b46d4eb13a0c5c91c09704fda0a89d09c4
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-16 10:14:29 +0100

    Add project files.

---
+++ inluppg7.7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace inluppg7_x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "a", "b", "b", "c", "hej" };
+            string[] array2 = { "hej", "a", "a", "b", "programmering" };
+
+            foreach (string s in FinnsIBada(array1, array2))
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string[] FinnsIBada(string[] array1, string[] array2)
+        {
+            Dictionary<string, int> repeterade =  new Dictionary<string, int>();
+            foreach (string s in array1)
+            {
+                repeterade[s] = 1;
+            }
+
+            int repeteradeAntal = 0;
+            foreach (string s in array2)
+            {
+                if (repeterade.ContainsKey(s))
+                {
+                    repeterade[s]++;
+                    repeteradeAntal++;
+                }
+            }
+            string[] returnArray = new string[repeteradeAntal];
+
+            int count = 0;
+            foreach(string s in repeterade.Keys)
+            {
+                if (repeterade[s] >= 2)
+                {
+                    returnArray[count] = s;
+                    count++;
+
+                }
+            }
+
+            return returnArray;
+        }
+    }
+}
